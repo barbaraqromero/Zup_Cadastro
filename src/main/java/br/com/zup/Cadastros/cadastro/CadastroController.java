07d00e32c0149f4e556e6a9b1cfa7d0b0eeb717c
Bárbara Romero <bqromero@gmail.com>
2021-11-09 07:40:29 -0300

    Criar método na controller que pesquisa por parâmetros

--- src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
+++ src/main/java/br/com/zup/Cadastros/cadastro/CadastroController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/cadastros")
 public class CadastroController {
@@ -15,6 +18,18 @@ public class CadastroController {
   public void cadastrarCliente(@RequestBody Cadastro cadastro) {
     cadastroService.cadastrarCliente(cadastro);
 
+  }
+
+  @GetMapping
+  public List<ResumoDTO> aplicarFiltros(@RequestParam(required = false) boolean moraSozinho,
+                                        @RequestParam(required = false) boolean temPet,
+                                        @RequestParam(required = false) Integer idade) {
+
+    List<ResumoDTO> resumo = new ArrayList<>();
+    for (Cadastro referencia : cadastroService.aplicarFiltros(moraSozinho, temPet, idade)) {
+      resumo.add(new ResumoDTO(referencia.getCpf(), referencia.getNome(), referencia.getSobrenome()));
+    }
+    return resumo;
 
   }
 
